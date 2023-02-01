a6755641a80e25fbae57f4e8fbcd57c0f3a5b9ff
Tony Peakman <dagger8224@gmail.com>
2023-02-02 00:18:40 +0800

    update +loading directive test case

--- function_tests/cases/directives/lifeCycles/+loading/async/test.js
+++ function_tests/cases/directives/lifeCycles/+loading/async/test.js
@@ -1,16 +1,10 @@
+const helper = (page, values = ['', '']) => Promise.all(values.map((value, index) => page.jQuery(`#span${ index + 1 }`).text().then(text => expect(text).toBe(value))));
+
 runner('+loading directive async test suite', describe, it, __dirname, async page => {
-    await page.jQuery('#span1').text().then(text => expect(text).toBe(''));
-    await page.jQuery('#span2').text().then(text => expect(text).toBe(''));
+    await helper(page);
     let resolve = null;
     let promise = new Promise(r => (resolve = r));
-    setTimeout(async () => {
-        await page.jQuery('#span1').text().then(text => expect(text).toBe(''));
-        await page.jQuery('#span2').text().then(text => expect(text).toBe(''));
-    }, 200);
-    setTimeout(async () => {
-        await page.jQuery('#span1').text().then(text => expect(text).toBe('text'));
-        await page.jQuery('#span2').text().then(text => expect(text).toBe('1'));
-        resolve(true);
-    }, 400);
+    setTimeout(() => helper(page), 200);
+    setTimeout(() => helper(page, ['text', '1']).then(resolve), 400);
     return promise;
 });
