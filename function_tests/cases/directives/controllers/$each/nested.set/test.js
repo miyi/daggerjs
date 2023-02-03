1c3bc5b3e7ce718ea1c94c342ec5452ba91a3294
dagger.core.lbb <dagger.core.lbb@gmail.com>
2023-02-03 13:47:52 +0800

    update test case for $each directive

---
+++ function_tests/cases/directives/controllers/$each/nested.set/test.js
@@ -0,0 +1,10 @@
+runner('$each directive nested.object test suite', describe, it, __dirname, page => {
+    const divs = page.jQuery('.each-div-layer1');
+    return [divs.then(divs => expect(divs.length).toBe(3)), async () => {
+        const spans = divs.find('span');
+        expect((await spans).length).toBe(6);
+        expect((await spans.eq(0).text())).toBe('0 - a - a - 0 - m - m');
+        expect((await spans.eq(2).text())).toBe('1 - b - b - 0 - m - m');
+        expect((await spans.eq(5).text())).toBe('2 - c - c - 1 - n - n');
+    }];
+});
