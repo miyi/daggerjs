483f7fb67940c87e15feb130b58458e2a14dd40f
miyi <mylegojesus@gmail.com>
2023-06-29 17:09:06 -0700

    not working, but not sure what the problem is

---
+++ test/router.js
@@ -0,0 +1,32 @@
+return {
+  mode: "history",
+  default: "/",
+  aliases: {
+    "/": "/nested/nested2/a",
+  },
+  modules: ["nested", "nested2", "nested3"],
+  routing: {
+    constants: {
+      title: "root",
+    },
+    children: [
+      {
+        path: "nested",
+        modules: "nested",
+        constants: { layer1: "nested", bgColor: "brown", title: "nested" },
+        children: [
+          {
+            path: "nested2",
+            modules: "nested2",
+            constants: {layer2: "nested2", bgColor: "grey", title: "nested2"},
+            chilren: [
+              {path: 'a', modules: "nested3", constants: {layer3: "nested3", bgColor: "red", title: "nested3 a"}},
+              {path: 'b', modules: "nested3", constants: {layer3: "nested3", bgColor: "yellow", title: "nested3 b"}},
+              {path: 'c', modules: "nested3", constants: {layer3: "nested3", bgColor: "green", title: "nested3 c"}},
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
